3d975f378446609893e770b16f762836268a61a0
Adrian T Capacite <45900436+AdrianCapacite@users.noreply.github.com>
2023-05-14 13:40:48 +0100

    Implemented Bubbl Sort

--- src/algorithms/sorting/BubbleSort.java
+++ src/algorithms/sorting/BubbleSort.java
@@ -1,21 +1,48 @@
 package algorithms.sorting;
 
-public class BubbleSort implements ISortingAlgorithm {
+import javax.xml.transform.SourceLocator;
+
+import console.Colors;
+
+public class BubbleSort implements ISortingAlgorithm, SampleArrays, Colors {
 
     public static void main(String[] args) {
-        System.out.println("==== BubbleSort demo ====");
+        System.out.println(PURPLE + "==== BubbleSort demo ====" + RESET);
+
+        TestSorting testSorting = new TestSorting();
+
+        testSorting.testSorting(new BubbleSort());
     }
 
+    /**
+     * Sorts an array using the bubble sort algorithm.
+     * Complexity: O(n^2)
+     `
+     * @param array The array to sort
+     * @return The sorted array
+     */
     @Override
     public void sort(int[] array) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'sort'");
+        // For each element in the array
+        for (int i = 0; i < array.length; i++) {
+            // Move the largest element to the end
+            boolean swapped = false;
+            for (int j = 1; j < array.length - i; j++) {
+                if (array[j - 1] > array[j]) {
+                    swap(array, j - 1, j);
+                    swapped = true;
+                }
+            }
+
+            if (!swapped) break;
+        }
     }
 
     @Override
     public void swap(int[] array, int i, int j) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'swap'");
+        int temp = array[j];
+        array[j] = array[i];
+        array[i] = temp;
     }
 
 }
