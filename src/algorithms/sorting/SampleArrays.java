b15f7ed82c50047a3926c041db5e4465705ed5a5
Adrian T Capacite <45900436+AdrianCapacite@users.noreply.github.com>
2023-05-14 13:41:08 +0100

    Sample Arrays

--- src/algorithms/sorting/SampleArrays.java
+++ src/algorithms/sorting/SampleArrays.java
@@ -9,4 +9,5 @@ public interface SampleArrays {
     static int[] random = {5,9,10,1,3,2,18,4,13,11,20,14,6,8,15,16,19,12,7,17};
 
     static int[] reverseOrdered = {20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1};
+
 }
