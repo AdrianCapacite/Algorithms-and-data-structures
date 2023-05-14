b3e4463fc9cb81272d61669527f4750852d3798a
Adrian T Capacite <45900436+AdrianCapacite@users.noreply.github.com>
2023-05-14 13:23:27 +0100

    Added sample arrays

---
+++ src/algorithms/sorting/SampleArrays.java
@@ -0,0 +1,12 @@
+package algorithms.sorting;
+
+public interface SampleArrays {
+
+    static int[] ordered = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+
+    static int[] almostSorted = {1,2,3,4,5,6,7,8,11,9,10,12,13,14,15,16,17,18,19,20};
+
+    static int[] random = {5,9,10,1,3,2,18,4,13,11,20,14,6,8,15,16,19,12,7,17};
+
+    static int[] reverseOrdered = {20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1};
+}
